7fec91ed18476c47255940f9efa271e4ad7c8c9b
sajib689 <jibonraja11@gmail.com>
2024-09-25 22:06:43 +0600

    add activity route on taskroute page

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post('/create', protectRoute, isAdminRoute, createTask)
 router.post('/duplicate/:id',protectRoute,isAdminRoute,duplicateTask)
-
+router.post('activity/:id')
 
 export default router;
