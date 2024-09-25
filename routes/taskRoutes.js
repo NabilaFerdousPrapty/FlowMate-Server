55b1505b5dc29a8fb27bff46675ee704b9fdf9ce
sajib689 <jibonraja11@gmail.com>
2024-09-25 22:05:46 +0600

    add duplicate task on taskroute page

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { duplicateTask } from "../controllers/taskController";
 const router = express.Router();
 
 router.post('/create', protectRoute, isAdminRoute, createTask)
-router.post('/duplicate/:id',)
+router.post('/duplicate/:id',protectRoute,isAdminRoute,duplicateTask)
 
 
 export default router;
