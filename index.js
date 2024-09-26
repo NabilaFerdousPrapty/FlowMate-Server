b8b7d7e1a3775f6ad1e5d9be845be57b6d250f3c
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-09-26 21:10:35 +0600

    [prapty] edits

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(
       'http://localhost:5173',
       'http://localhost:5174',
       'https://flowmate-letscollaborate.web.app/',
+      'https://flowmate-letscollaborate.firebaseapp.com/'
       
     
     ],
