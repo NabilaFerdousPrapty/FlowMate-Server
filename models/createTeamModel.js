e9c1ce77bb9416103422c177cdb1b393658e094b
sajib689 <jibonraja11@gmail.com>
2024-10-02 12:52:59 +0600

    chnage some code on the models folder

--- models/createTeamModel.js
+++ models/createTeamModel.js
@@ -1,5 +1,5 @@
 const { db } = require("../utils/db");
 
-const contactCollection = db.collection("create-team");
+const createTeamCollection = db.collection("create-team");
 
-module.exports = contactCollection;
+module.exports = createTeamCollection;
