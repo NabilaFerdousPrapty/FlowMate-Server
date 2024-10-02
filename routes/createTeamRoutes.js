b229aa7e88f39cc788f04e0338b0bfc4ce4df192
sajib689 <jibonraja11@gmail.com>
2024-10-02 16:00:30 +0600

    add the post method and the get method on the createTeamRoutes

--- routes/createTeamRoutes.js
+++ routes/createTeamRoutes.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const {createTeam,getTeamByEmail} = require("../controllers/createTeamController.js")
 const router = express.Router();
+router.post("/", createTeam);
+router.get("/", getTeamByEmail);
 
+module.exports = router;
