ddcbb4b53246248e6686856661b87302cbc7a759
git12nazneen <lipinazneen65@gmail.com>
2024-10-03 04:27:11 +0600

    [lipi] get id , put, patch route ready

--- routes/createTaskRoutes.js
+++ routes/createTaskRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { createTask, getTask } = require("../controllers/createTaskController");
+const { createTask, getTask, updateTask, deleteTask, specificTask, updateOneTask } = require("../controllers/createTaskController");
 
 const router = express.Router();
 
 router.post("/",createTask );
 router.get("/", getTask);
+router.get("/:id", specificTask);
+
+router.delete("/:id", deleteTask);;
+router.put("/:id", updateOneTask);;
+router.patch("/:id", updateTask);;
 
 module.exports = router;
 
