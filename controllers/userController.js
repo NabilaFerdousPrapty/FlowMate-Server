715c62e6edfc309cccab8356a767d0e7f1485c26
smais007 <smaisshawon@gmail.com>
2024-10-03 18:48:32 +0600

    impliment JWT

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,66 @@
 const usersCollection = require("../models/userModel");
 
+const Users = require("../models/userModel");
+const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 exports.createUser = async (req, res) => {
   try {
-    const query = req.body;
-    const email = query?.email;
+    const { name, email, password, role } = req.body;
     const isExiting = await usersCollection.findOne({ email });
 
     if (isExiting) {
       return res.send({ message: "User already exists" });
     }
 
-    const result = await usersCollection.insertOne(query);
+    const hasedPassword = await bycrypt.hash(password, 10);
+
+    const result = await usersCollection.insertOne({
+      name,
+      email,
+      password: hasedPassword,
+      role,
+    });
+
     res.send(result);
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).send({ message: "Failed to create user" });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await usersCollection.findOne({ email });
+
+    if (!user) {
+      return res.status(401).send({ message: "User not found" });
+    }
+
+    const isMatch = await bycrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).send({ message: "Login successful", token });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).send({ message: "Failed to fetch users" });
+  }
+};
+
 exports.getUsers = async (req, res) => {
   try {
-
     const result = await usersCollection.find().toArray();
     res.send(result);
   } catch (error) {
