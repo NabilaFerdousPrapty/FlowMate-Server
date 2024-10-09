640abc1bff585bf635c226478fee132d3f655611
sajib689 <jibonraja11@gmail.com>
2024-10-09 13:04:51 +0600

    add

---
+++ routes/createTaskBoardRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { createBoard, getBoard, specificBoard, getEmailBoard } = require("../controllers/boardController");
+
+const router = express.Router();
+
+router.post("/",createBoard );
+router.get("/", getBoard);
+router.get("/:id", specificBoard);
+router.get("/:email", getEmailBoard);
+
+module.exports = router;
+
+// local:5000/createBoard
+// local:5000/getBoard
